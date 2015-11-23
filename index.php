b6198177e6db2037f25c8b6a2a79600d2699adfc
Viktor Tandit <dragonmuonline@mail.ru>
2015-11-23 13:35:11 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+
+//Контроллер
+$houses = objectFabrica::getList(array('type'=>'house','price'...далее));
+
+//Все элементы в 1 пачке
+$objects = objectFabrica::getList(array('price'...далее));
